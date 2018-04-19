4a769887e3b2f5458b5a52190b15d365d50abd61
Andy Campbell <andy.campbell@qwizdom.co.uk>
2018-04-19 11:47:35 +0100

    rebuilt

--- src/plugin/components/dialog-window.vue
+++ src/plugin/components/dialog-window.vue
@@ -7,67 +7,63 @@
 
         <transition :name="animation" @after-leave="animationEnded('content')" appear>
             <div class="modal-card" @click="closeAtOutsideClick">
-                <div class="dg-content-cont dg-content-cont--floating">
-                    <div class="dg-main-content" @click.stop>
+              <div @click.stop>
+								<template v-if="messageHasTitle">
+									<div class="modal-class-head">
+										<h6 v-if="options.html" class="dg-title" v-html="messageTitle"></h6>
+										<h6 v-else class="dg-title">{{ messageTitle }}</h6>
+									</div>
+								</template>
+								<div class="modal-card-body">
+										<div v-if="options.html" class="dg-content" v-html="messageBody"></div>
+										<div v-else class="dg-content">{{ messageBody }}</div>
 
-                        <div :class="['dg-content-body', {'dg-content-body--has-title': messageHasTitle}]">
-                            <template v-if="messageHasTitle">
-                                <h6 v-if="options.html" class="dg-title" v-html="messageTitle"></h6>
-                                <h6 v-else class="dg-title">{{ messageTitle }}</h6>
-                            </template>
+										<form v-if="isHardConfirm || isPrompt"
+													class="dg-form"
+													autocomplete="off"
+													@submit.prevent="submitDialogForm">
+												<label for="dg-input-elem" style="font-size: 13px">{{ hardConfirmHelpText }}</label>
+												<input type="text" :placeholder="options.verification"
+																v-model="input"
+																autocomplete="off"
+																id="dg-input-elem"
+																ref="inputElem"
+																style="width: 100%;margin-top: 10px;
+												padding: 5px 15px; font-size: 16px;border-radius: 4px; border: 2px solid #eee"/>
+										</form>
+											<div class="dg-content-footer">
+												<div class="field is-grouped">
+													<p class="control">
+														<a class="button" 
+																:is="leftBtnComponent" 
+																:loading="loading"
+																:enabled="leftBtnEnabled" 
+																:options="options" 
+																:focus="leftBtnFocus"
+																@click="clickLeftBtn()"> 
+																<span v-if="options.html" v-html="leftBtnText"></span>
+																<span v-else>{{ leftBtnText }}</span>
+														</a>
+													</p>
 
-                            <div v-if="options.html" class="dg-content" v-html="messageBody"></div>
-                            <div v-else class="dg-content">{{ messageBody }}</div>
-
-                            <form v-if="isHardConfirm || isPrompt"
-                                  class="dg-form"
-                                  autocomplete="off"
-                                  @submit.prevent="submitDialogForm">
-                                <label for="dg-input-elem" style="font-size: 13px">{{ hardConfirmHelpText }}</label>
-                                <input type="text" :placeholder="options.verification"
-                                       v-model="input"
-                                       autocomplete="off"
-                                       id="dg-input-elem"
-                                       ref="inputElem"
-                                       style="width: 100%;margin-top: 10px;
-                               padding: 5px 15px; font-size: 16px;border-radius: 4px; border: 2px solid #eee"/>
-                            </form>
-                        </div>
-
-                        <div class="dg-content-footer">
-													<div class="field is-grouped">
-														<p class="control">
-															<a class="button" 
-																 :is="leftBtnComponent" 
-																 :loading="loading"
-																 :enabled="leftBtnEnabled" 
-																 :options="options" 
-																 :focus="leftBtnFocus"
-																 @click="clickLeftBtn()"> 
-																	<span v-if="options.html" v-html="leftBtnText"></span>
-																	<span v-else>{{ leftBtnText }}</span>
-															</a>
-														</p>
-
-														<p class="control">
-															<a class="button" 
-																 :is="rightBtnComponent" 
-																 :loading="loading"
-																 :enabled="rightBtnEnabled" 
-																 :options="options" 
-																 :focus="rightBtnFocus"
-																 @click="clickRightBtn()">
-																	<span v-if="options.html" v-html="rightBtnText"></span>
-																	<span v-else>{{ rightBtnText }}</span>
-															</a>
-														</p>
-													</div>
-
-                          <div class="dg-clear"></div>
-                        </div>
-                    </div>
+													<p class="control">
+														<a class="button" 
+																:is="rightBtnComponent" 
+																:loading="loading"
+																:enabled="rightBtnEnabled" 
+																:options="options" 
+																:focus="rightBtnFocus"
+																@click="clickRightBtn()">
+																<span v-if="options.html" v-html="rightBtnText"></span>
+																<span v-else>{{ rightBtnText }}</span>
+														</a>
+													</p>
+												</div>
+                        <div class="dg-clear"></div>
+                      </div>
                 </div>
-            </div>
+              </div>
+						</div>
         </transition>
     </div>
 </template>
